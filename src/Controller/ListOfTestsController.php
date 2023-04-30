329fa283932e0eb6c0cdf1f7393a7fb4fcaf4bdf
YassineMnasri223 <yassine.mnasri@esprit.tn>
2023-04-30 18:49:27 +0100

    lkdim kima jdid

--- src/Controller/ListOfTestsController.php
+++ src/Controller/ListOfTestsController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 class ListOfTestsController extends AbstractController
@@ -54,7 +55,7 @@ public function pass(Test $test, QuestionRepository $questionRepository):Respons
     ]);
 }
 #[Route('/submit-answers/{id}', name: 'app_submit_answers', methods: ['POST'])]
-public function submitAnswers(Request $request, Test $test, QuestionRepository $questionRepository): Response
+public function submitAnswers(Request $request, Test $test, QuestionRepository $questionRepository,SessionInterface $session): Response
 {
     $userAnswers = $request->request->all();
     $questions = $questionRepository->findByTestId($test->getId());
@@ -74,10 +75,39 @@ public function submitAnswers(Request $request, Test $test, QuestionRepository $
     }
 
     $finalScore = round(($score / count($questions)) * 100);
-    // Generate the PDF
-    $options = new Options();
-    $options->set('defaultFont', 'Arial');
-    $dompdf = new Dompdf($options);
+    $session = $request->getSession();
+    $session->set('userAnswers', $userAnswers);
+    $session->set('score', $score);
+    $session->set('correctAnswers', $correctAnswers);
+
+    return $this->render('list_of_tests/results.html.twig', [
+        'test' => $test,
+        'questions' => $questions,
+        'userAnswers' => $userAnswers,
+        'finalScore' => $finalScore,
+        'correctAnswers' => $correctAnswers,
+    ]);
+}
+#[Route('/generate-pdf/{id}', name: 'app_generate_pdf', methods: ['GET'])]
+public function generatePdf(Request $request,Test $test, QuestionRepository $questionRepository): Response
+{
+    $questions = $questionRepository->findByTestId($test->getId());
+
+    $session = $request->getSession();
+    $userAnswers = $session->get('userAnswers');
+    $score = $session->get('score');
+    $correctAnswers = $session->get('correctAnswers');
+
+    foreach ($questions as $question) {
+        $userAnswer = $request->request->get('answer_'.$question['id']);
+        if ($userAnswer === $question['respone']) {
+            $score++;
+            $correctAnswers++;
+        }
+        $userAnswers[$question['quest']] = $userAnswer;
+    }
+
+    $finalScore = round(($score / count($questions)) * 100);
 
     $html = $this->renderView('list_of_tests/pdf_template.html.twig', [
         'test' => $test,
@@ -87,26 +117,25 @@ public function submitAnswers(Request $request, Test $test, QuestionRepository $
         'correctAnswers' => $correctAnswers,
     ]);
 
+    $options = new Options();
+    $options->set('defaultFont', 'Arial');
+
+    $dompdf = new Dompdf($options);
     $dompdf->loadHtml($html);
     $dompdf->setPaper('A4', 'portrait');
     $dompdf->render();
 
-    $response = new Response($dompdf->output());
-    $disposition = $response->headers->makeDisposition(
-        ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-        'test_result.pdf'
-    );
-    $response->headers->set('Content-Disposition', $disposition);
+    $pdfOutput = $dompdf->output();
+    $filename = $test->getName() . ' Results.pdf';
+
+    $response = new Response($pdfOutput);
+    $response->headers->set('Content-Type', 'application/pdf');
+    $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '"');
+    $response->headers->set('Content-Length', strlen($pdfOutput));
 
     return $response;
-    return $this->render('list_of_tests/results.html.twig', [
-        'test' => $test,
-        'questions' => $questions,
-        'userAnswers' => $userAnswers,
-        'finalScore' => $finalScore,
-        'correctAnswers' => $correctAnswers,
-    ]);
 }
+
 #[Route('/recherche_ajax', name: 'recherche_ajax_test')]
     public function rechercheAjax(Request $request): JsonResponse
     {
